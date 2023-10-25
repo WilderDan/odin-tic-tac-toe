c0d513c52b92432b06548baf2785b47a641bbd8c
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-10-25 14:46:30 -0500

    Add game board

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("app.js");
+(function App() {
+  const Game = (function () {
+    const board = new Array(9).fill("");
+    const getBoard = () => board;
+    const selectCell = (index) => (board[index] = "X");
+    return { getBoard, selectCell };
+  })();
+
+  // DOM cache
+  const gameBoardCellElems = Array.from(
+    document.getElementsByClassName("gameBoard__cell")
+  );
+
+  // Event Binding
+  gameBoardCellElems.forEach((cell) =>
+    cell.addEventListener("click", handleClick)
+  );
+
+  // Render
+  function render() {
+    gameBoardCellElems.forEach(
+      (cell, index) => (cell.innerHTML = Game.getBoard()[index])
+    );
+  }
+
+  // Other Functions
+  function handleClick(e) {
+    const index = e.target.dataset.index;
+    Game.selectCell(index);
+    render();
+  }
+
+  render();
+})();
